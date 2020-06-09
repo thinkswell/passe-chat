e3c4eab4db633709861ab7d6078bec60ac9ac7c8
Krishna Vishwakarma <krishna23712@gmail.com>
2020-06-10 00:52:13 +0530

    cross button added to room header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
-export default function Header({ room}) {
+export default function Header({ room }) {
   return (
     <header className="chat__head">
       {room}
+      <Link to="/">
+        <div className="chat__head--cross">X</div>
+      </Link>
     </header>
   );
 }
